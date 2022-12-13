049b025fde32989d43f0c5b169aa629f0dca7bd1
FeelProud <feelproud@protonmail.com>
2022-12-13 17:18:15 +0100

    Update autoconfig.sh

--- autoconfig.sh
+++ autoconfig.sh
@@ -30,6 +30,8 @@ echo ""
 apt-get update
 apt-get upgrade
 
+# Following the doc https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&Distribution=Ubuntu&target_version=22.04&target_type=deb_local
+
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
 mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
 wget https://developer.download.nvidia.com/compute/cuda/12.0.0/local_installers/cuda-repo-ubuntu2204-12-0-local_12.0.0-525.60.13-1_amd64.deb
