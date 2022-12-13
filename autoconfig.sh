abc6d833676a7799a4a54be83967098f996920af
FeelProud <feelproud@protonmail.com>
2022-12-13 17:13:14 +0100

    Update autoconfig.sh
    Add CUDA configuration

--- autoconfig.sh
+++ autoconfig.sh
@@ -12,6 +12,9 @@ target_term=lxterminal
 target_user=marc
 target_browser=brave-browser
 
+sudo apt-get update
+sudo apt-get upgrade
+
 # intro
 echo ""
 echo "--------------------------------------------------"
@@ -20,6 +23,23 @@ echo "   This script need to run with root privileges."
 echo "Please use this with a Ubuntu/Debian based distro."
 echo "--------------------------------------------------"
 echo ""
+
+echo ""
+echo "--------------------------------------------------"
+echo "      - Installing Nvidia drivers and CUDA -"
+echo "--------------------------------------------------"
+echo ""
+
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
+sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/12.0.0/local_installers/cuda-repo-ubuntu2204-12-0-local_12.0.0-525.60.13-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2204-12-0-local_12.0.0-525.60.13-1_amd64.deb
+sudo cp /var/cuda-repo-ubuntu2204-12-0-local/cuda-*-keyring.gpg /usr/share/keyrings/
+
+sudo apt-get update
+sudo apt-get -y install cuda
+
+sudo ln -s /usr/local/cuda-12.0/bin/* /usr/bin
 
 echo ""
 echo "--------------------------------------------------"
