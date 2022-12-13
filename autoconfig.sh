e529a3c9fe5d83d362360049faa9d151400aced8
FeelProud <feelproud@protonmail.com>
2022-12-13 18:02:45 +0100

    Update autoconfig.sh

--- autoconfig.sh
+++ autoconfig.sh
@@ -87,11 +87,11 @@ rm discord.deb
 
 # install visual studio code
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
-sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt install apt-transport-https
-sudo apt update
-sudo apt install code
+install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+apt install apt-transport-https
+apt update
+apt install code
 
 echo ""
 echo "--------------------------------------------------"
