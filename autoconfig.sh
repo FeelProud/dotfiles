7b56f70eb4b4039327bcdebc6ca9a706e3585406
FeelProud <feelproud@protonmail.com>
2022-12-13 21:53:18 +0100

    Update autoconfig.sh

--- autoconfig.sh
+++ autoconfig.sh
@@ -180,6 +180,8 @@ git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
 echo '. $HOME/.asdf/asdf.sh' >> ~/.bashrc
 echo '. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
 
+source ~/.bashrc
+
 echo ""
 echo "--------------------------------------------------"
 echo "                - Other configs -"
