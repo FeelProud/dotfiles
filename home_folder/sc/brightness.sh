f911d0a66e9d65666779b76928be6cdaba8358eb
FeelProud <feelproud@protonmail.com>
2022-12-15 22:25:38 +0100

    Update brightness.sh

--- home_folder/sc/brightness.sh
+++ home_folder/sc/brightness.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 # base dir for backlight class
-basedir="/sys/class/backlight/"
-
-# get the backlight handler
-handler=$basedir$(ls $basedir)"/"
+basedir="/sys/class/backlight/nvidia_wmi_ec_backlight/"
 
 # get current brightness
-old_brightness=$(cat $handler"brightness")
+old_brightness=$(cat $basedir"brightness")
 
 # get max brightness
-max_brightness=$(cat $handler"max_brightness")
+max_brightness=$(cat $basedir"max_brightness")
 
 # get current brightness %
 old_brightness_p=$(( 100 * $old_brightness / $max_brightness ))
@@ -22,6 +19,5 @@ new_brightness_p=$(($old_brightness_p $1))
 new_brightness=$(( $max_brightness * $new_brightness_p / 100 ))
 
 # set the new brightness value
-chmod 666 $handler"brightness"
-echo $new_brightness > $handler"brightness"
-
+chmod 666 $basedir"brightness"
+echo $new_brightness > $basedir"brightness"
