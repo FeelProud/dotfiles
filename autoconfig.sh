a44e8e68b739cd717e0522108482d89687fbee59
FeelProud <feelproud@protonmail.com>
2022-12-16 23:24:37 +0100

    Update autoconfig.sh

--- autoconfig.sh
+++ autoconfig.sh
@@ -231,8 +231,8 @@ snap install vagrant --classic
 # ASDF
 apt install curl git
 git clone https://github.com/asdf-vm/asdf.git /home/${target_user}/.asdf --branch v0.10.2
-echo '. /home/${target_user}/.asdf/asdf.sh' >> ~/.bashrc
-echo '. /home/${target_user}/.asdf/completions/asdf.bash' >> ~/.bashrc
+echo '. /home/${target_user}/.asdf/asdf.sh' >> /home/${target_user}/.bashrc
+echo '. /home/${target_user}/.asdf/completions/asdf.bash' >> /home/${target_user}/.bashrc
 
 source ~/.bashrc
 
