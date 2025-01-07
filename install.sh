88e8949909c0110592b054965fb25f8abb82a12a
FeelProud <feelproud@protonmail.com>
2025-01-07 10:29:18 +0100

    Removed tee

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ if [ ! "$(command -v paru)" ]; then
 
   git clone https://aur.archlinux.org/paru-bin.git || { printf "%s - Failed to clone paru-bin from AUR\n" "${ERROR}"; exit 1; }
   cd paru-bin || { printf "%s - Failed to enter paru directory\n" "${ERROR}"; exit 1; }
-  makepkg -si --noconfirm 2>&1 | tee -a "$LOG" || { printf "%s - Failed to install paru-bin from AUR\n" "${ERROR}"; exit 1; }
+  makepkg -si --noconfirm 2>&1 || { printf "%s - Failed to install paru-bin from AUR\n" "${ERROR}"; exit 1; }
   paru -Syu --noconfirm 2>&1 || { printf "%s - Failed to update system\n" "${ERROR}"; exit 1; }
   cd "$original_dir"
 fi
